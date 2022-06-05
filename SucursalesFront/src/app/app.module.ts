22486958750b3590caab458d1d593111b90f6bc5
Andy Cuyuch <acuyuch-2017403@kinal.edu.gt>
2022-06-05 13:36:21 -0600

    Se crearon los components de login y register

--- SucursalesFront/src/app/app.module.ts
+++ SucursalesFront/src/app/app.module.ts
@@ -6,13 +6,17 @@ import { AppComponent } from './app.component';
 import { NavbarComponent } from './components/navbar/navbar.component';
 import { FooterComponent } from './components/footer/footer.component';
 import { HomeComponent } from './components/home/home.component';
+import { LoginComponent } from './components/login/login.component';
+import { RegisterComponent } from './components/register/register.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     NavbarComponent,
     FooterComponent,
-    HomeComponent
+    HomeComponent,
+    LoginComponent,
+    RegisterComponent
   ],
   imports: [
     BrowserModule,
