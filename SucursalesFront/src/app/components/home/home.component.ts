c95c0e81f7076ead4604e7824bd89833fe87e826
Barillas <jbarillas-2017174@kinal.edu.gt>
2022-06-09 12:52:38 -0600

    Se arreglaron las vistas

--- SucursalesFront/src/app/components/home/home.component.ts
+++ SucursalesFront/src/app/components/home/home.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { CompanyModel } from 'src/app/models/company.model';
+import { CompanyRestService } from 'src/app/services/companyRest/company-rest.service';
 
 @Component({
   selector: 'app-home',
@@ -6,10 +8,24 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./home.component.css']
 })
 export class HomeComponent implements OnInit {
+  company: CompanyModel
 
-  constructor() { }
+  constructor(
+    private companyRest: CompanyRestService
+  ) {
+    this.company = new CompanyModel('','','','','','')
+   }
 
   ngOnInit(): void {
+    this.getCompany();
   }
 
+  getCompany() {
+    this.companyRest.getIdCompany(this.companyRest.getIdentity()._id).subscribe({
+      next: (res: any) => {
+        this.company = res.company
+      },
+      error: (err) => alert(err.error.message)
+    })
+  }
 }
