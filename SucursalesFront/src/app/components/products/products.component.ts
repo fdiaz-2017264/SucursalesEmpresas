07b687e292ad650fdffa585e592ebeceb0b24a7e
Oscar <odelcid-2017173@kinal.edu.gt>
2022-06-11 13:00:54 -0600

    Se creo post Productos

--- SucursalesFront/src/app/components/products/products.component.ts
+++ SucursalesFront/src/app/components/products/products.component.ts
@@ -3,79 +3,98 @@ import { ProductsModel } from 'src/app/models/product.model';
 import { ProductRestService } from 'src/app/services/productRest/product-rest.service';
 import { CompanyModel } from 'src/app/models/company.model';
 import { CompanyRestService } from 'src/app/services/companyRest/company-rest.service';
-
+import { ProductSRestService } from 'src/app/services/productS/product-srest.service';
+import { ProductSsModel } from 'src/app/models/prodctS.model';
 
 @Component({
   selector: 'app-products',
   templateUrl: './products.component.html',
-  styleUrls: ['./products.component.css']
+  styleUrls: ['./products.component.css'],
 })
-
 export class ProductsComponent implements OnInit {
   products: any;
   product: ProductsModel;
   productU: any;
   company: CompanyModel;
+  value: any = [];
+  productModel: ProductSsModel;
 
   constructor(
-      private productRest: ProductRestService, private companyRest:CompanyRestService
-  ) { 
-    this.product = new ProductsModel('','',0,'');
-    this.company = new CompanyModel('','','','','','');
+    private productRest: ProductRestService,
+    private companyRest: CompanyRestService,
+    private productSRes: ProductSRestService
+  ) {
+    this.product = new ProductsModel('', '', 0, '');
+    this.company = new CompanyModel('', '', '', '', '', '');
+    this.productModel = new ProductSsModel('', 0 , '', '');
+    
   }
 
-  
   ngOnInit(): void {
     this.getProducts();
     this.getCompany();
   }
 
-  getCompany(){
-    this.companyRest.getIdCompany(this.companyRest.getIdentity()._id).subscribe({
-      next: (res:any) =>{this.company = res.company},
-      error: (err)=> alert(err.error.message)
-    })
+  getCompany() {
+    this.companyRest
+      .getIdCompany(this.companyRest.getIdentity()._id)
+      .subscribe({
+        next: (res: any) => {
+          this.company = res.company;
+        },
+        error: (err) => alert(err.error.message),
+      });
   }
 
-  getProducts(){
+  getProducts() {
     this.productRest.getProducts().subscribe({
-      next: (res:any) => this.products = res.searchProduct ,
-      error: (err)=> console.log(err)
-    })
+      next: (res: any) => (this.products = res.searchProduct),
+      error: (err) => console.log(err),
+    });
   }
 
-  saveProduct(saveProductForm: any){
+  saveProduct(saveProductForm: any) {
     this.productRest.saveProduct(this.product).subscribe({
-      next: (res:any)=>{
+      next: (res: any) => {
         this.getProducts();
         saveProductForm.reset();
       },
-      error: (err)=> alert(err.error.message || err.error)
-    })
+      error: (err) => alert(err.error.message || err.error),
+    });
   }
- 
-  deleteProduct(id:string){
+
+  deleteProduct(id: string) {
     this.productRest.deleteProduct(id).subscribe({
-      next: (res:any)=>{
+      next: (res: any) => {
         this.getProducts();
       },
-      error: (err)=> console.log(err)
-    })
+      error: (err) => console.log(err),
+    });
   }
 
-  getProduct(productId:string){
+  getProduct(productId: string) {
     this.productRest.getProduct(productId).subscribe({
-      next: (res:any)=> this.productU = res.searchProduct,
-      error: (err)=> console.log(err)
-    })
+      next: (res: any) => (this.productU = res.searchProduct),
+      error: (err) => console.log(err),
+    });
   }
 
-  updateProduct(){
+  updateProduct() {
     this.productRest.updateProduct(this.productU._id, this.productU).subscribe({
-      next: (res:any)=>{
+      next: (res: any) => {
         this.getProducts();
       },
-      error: (err)=> console.log(err)
+      error: (err) => console.log(err),
+    });
+  }
+
+  exportProduct(){
+    this.productSRes.saveProduct(this.productU._id, this.productModel).subscribe({
+      next: (res: any)=>{
+        alert(res.productoS)
+      },
+      error: (err) => console.log(err.error.message),
+    
     })
   }
 }
---
+++ SucursalesFront/src/app/models/prodctS.model.ts
@@ -0,0 +1,8 @@
+export class ProductSsModel{
+    constructor(
+        public name: string,
+        public stock: number,
+        public cantidadV: string,
+        public idSucursal: string
+    ){}
+}
